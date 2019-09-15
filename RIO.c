ceafc40935cee5f6f4e9c61137ed1fed7a322842
qianyuqiao <18302996874@163.com>
2019-09-15 22:04:12 +0800

    Update RIO.c

--- RIO.c
+++ RIO.c
@@ -17,19 +17,24 @@ ssize_t rio_readn(int fd, void* buf, size_t n)
 	char *bufp = buf;
 	while (left > 0)
 	{
-		if ((nread = read(fd, bufp, left)) < 0)
+		nread = read(fp, bufp, left);
+		if (nread > 0)
+		{
+			left -= nread;
+			butp += nread;		
+		}
+		else if (nread < 0)
 		{
 			if (errno == EINTR)
 				nread = 0;
 			else
 				return -1;
 		}
-		else if (nread == 0)
+		else (nread == 0)
 			break;
-		left -= nread;
-		butp += nread;
+
 	}
-	return n-left;
+	return n - left;
 }
 
 
@@ -40,16 +45,112 @@ ssize_t rio_writen(int fd, void* buf, size_t n)
 	char *bufp = buf;
 	while (left > 0)
 	{
-		if ((nwrite = write(fd, bufp, left)) <= 0)
+		nwrite = write(fd, bufp, left)
+		if (nwrite > 0)
+		{
+			// 由于必须将起始地址之后n个字节的数据写到缓冲区，所以必须等写完了才能返回
+			left -= nwrite;
+			butp += nwrite;
+		}
+		else
 		{ 
 			if (errno == EINTR)
 				nwrite = 0;
 			else
 				return -1;
 		}
-		// 由于必须将起始地址之后n个字节的数据写到缓冲区，所以必须等写完了才能返回
-		left -= nwrite;
-		butp += nwrite;
+
 	}
 	return n-left;
+}
+
+2.带缓冲区的输入函数
+为什么要带缓冲区呢？这是因为可能存在下面这种情况：需要统计一个文本文件的行数，如果单纯的重复调用read系统调用每次
+读取一个字节读到换行符统计的话可能会因为上下文的切换造成大量的开销。但是如果采用一个缓冲区，在读取之前先读进来一段
+文本到缓冲区中的话可以显著的降低read系统调用的次数从而提高性能。
+其中最重要的是一个数据结构和三个函数
+
+数据结构
+typedef struct {
+    int rio_fd;                //与内部缓冲区关联的描述符
+    int rio_cnt;               //缓冲区中剩下的字节数
+    char *rio_bufp;          //指向缓冲区中下一个未读的字节
+    char rio_buf[RIO_BUFSIZE]; 
+} rio_t;
+
+static ssize_t rio_read(rio_t *rp, char *usrbuf, size_t n)
+{
+	if (rp->rio_cnt == 0)
+	{
+		while (rp->rio_cnt <= 0)
+		{
+			rp->rio_cnt = read(rp->rio_fd, rp->rio_buf, RIO_BUFSIZE)
+			if (rp->rio_cnt > 0)
+			{
+				rp->rio_bufp = rp->rio_buf;			
+			}
+			else if (rp->rio_cnt < 0)
+			{
+				if (errno != EINTR) return -1;
+			}
+			else return 0;
+		}
+	}// 这里没有考虑的很复杂，没有继续上次的位置而是重新read
+	int cnt = n;
+	if (rp->rio_cnt < n) cnt = rp->rio_cnt;
+	memcpy(usrbuf, rp->rio_bufp, cnt);
+	rp->rio_cnt -= cnt;
+	rp->rio_bufp += cnt;
+	return cnt;
+}
+
+ssize_t rio_readnb(rio_t *rp, void *usrbuf, size_t n) 
+{
+	size_t left = n;
+	ssize_t readn;
+	char* buf = usrbuf;
+	while (left > 0)
+	{
+		readn = rio_read(rp, usrbuf, n);
+		if (readn > 0)
+		{
+			left -= readn;
+			buf += readn;
+		}
+		else if (readn < 0) return -1;
+		else (readn == 0) break;
+	}
+	return n - left;
+}
+
+ssize_t rio_readlineb(rio_t *rp, void *usrbuf, size_t maxlen) // 在maxlen或者\n处终止
+{
+	int n;
+	int nread;
+	char c;
+	char* buf = usrbuf;
+	for (int n = 1; n < maxlen; n++)
+	{
+		nread = rio_read(rp->fd, &c, 1);
+		if (nread == 1)
+		{
+			*buf = c;
+			buf++;
+			if (c == '\n')
+			{
+				n++;
+				break;
+			}
+		}	
+		else if (nread == 0)
+		{
+			if (n == 1) return 0;
+			else break;
+		}
+		else
+			return -1;
+
+	}
+	*buf = 0;
+	return n - 1;
 }
