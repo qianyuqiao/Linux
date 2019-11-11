cdde3676e4cb9214f432b4bded590ff324fe400b
qianyuqiao <18302996874@163.com>
2019-11-11 15:55:42 +0800

    Create list_for_each_entry.c

---
+++ list_for_each_entry.c
@@ -0,0 +1,12 @@
+#define list_for_each_entry(pos, head, member)				\
+	for (pos = list_entry((head)->next, typeof(*pos), member);	\
+	     prefetch(pos->member.next), &pos->member != (head); 	\
+	     pos = list_entry(pos->member.next, typeof(*pos), member))
+
+
+#define list_entry(ptr_of_member, type, member) \
+	container_of(ptr_of_member, type, member)
+注:
+1.container_of的作用是根据结构体中某个指针的地址获取该结构体的地址
+2.prefetch的含义是告诉cpu那些元素有可能马上就要用到，告诉cpu预取一下，这样可以提高速度，用于预取以提高遍历速度
+3.对于pos = list_entry(pos->member.next, typeof(*pos), member)),首先pos->member.next的结合顺序是从左到右,将head移到下一个再取结构体
