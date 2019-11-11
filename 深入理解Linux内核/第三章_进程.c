df886a669526ae76ba30258f2f2bd3b75830acd5
qianyuqiao <18302996874@163.com>
2019-11-11 21:09:31 +0800

    Update and rename 第三章进程.c to 第三章_进程.c

--- 深入理解Linux内核/第三章_进程.c
+++ 深入理解Linux内核/第三章_进程.c
@@ -127,5 +127,48 @@ struct task_struct *pid_task(struct pid *pid, enum pid_type type)
 然后，#define hlist_entry(ptr, type, member) container_of(ptr,type,member)即通过task_struct中的hlist_node结构获得task_struct
 
 
-总结: 通过pid号码找到task_struct的流程：最重要的是hlist_head pid_hash
+总结: 
+通过pid号码找到task_struct的流程：最重要的是hlist_head pid_hash
 pid_number -> pid_chain -> upid -> pid -> hlist_head -> hlist_node -> task_struct
+
+3. fork(), vfork()和clone()
+fork()与vfork()都是通过调用clone()实现的
+int clone(int (*fn)(void *), void *child_stack, int flags, void *arg);
+相同点：child_stack等于父进程当前的栈指针,SIGCHLD
+区别:
+fork():其他clone标志位全部为0
+vfork():CLONE_VM和CLONE_VFORK
+e.g:
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <sched.h>
+#define FIBER_STACK 8192
+int a;
+void * stack;
+int do_something(){
+		a=10;
+		printf("This is son, the pid is:%d, the a is: %d\n", getpid(), a);
+		free(stack); 
+		exit(1);
+}
+int main() {
+		void * stack;
+		a = 1;
+		stack = malloc(FIBER_STACK);//为子进程申请系统堆栈
+		if(!stack) {
+				printf("The stack failed\n");
+				exit(0);
+		}
+		printf("creating son thread!!!\n");
+		clone(&do_something, (char *)stack + FIBER_STACK, CLONE_VM|CLONE_VFORK, 0);//创建子线程
+		printf("This is father, my pid is: %d, the a is: %d\n", getpid(), a);
+		exit(1);
+}
+
+son的PID：10692；
+
+father的PID：10691；
+
+parent和son中的a都为10；所以证明他们公用了一份变量a，是指针的复制，而不是值的复制
