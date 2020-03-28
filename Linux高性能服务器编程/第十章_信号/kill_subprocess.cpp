83df7bd3ca2d9d81de89b7033af2e4529595120d
qianyuqiao <18302996874@163.com>
2020-03-29 00:19:46 +0800

    Create kill_subprocess.cpp

---
+++ Linux高性能服务器编程/第十章_信号/kill_subprocess.cpp
@@ -0,0 +1,34 @@
+### 父进程杀死子进程
+```
+#include <iostream>
+#include <cstdlib>
+#include <unistd.h>
+#include <signal.h>
+using namespace std;
+int main()
+{
+    pid_t pid;
+    pid = fork();
+    int ret;
+    if (pid < 0)
+    {
+        cout << "create subprocess failed" << endl;
+        exit(-1);
+    }
+    else if (pid > 0)
+    {
+        sleep(1);
+        cout << "parent process" << endl;
+        kill(pid, 9);
+        cout << "kill " << pid << endl;
+        exit(0);
+    }
+    else
+    {
+        cout << "child process" << endl;
+        sleep(20);
+        exit(0);
+    }
+    return 0;
+}
+```
