890f67d20da594e566c0c2023fa2a9b0f4766038
qianyuqiao <18302996874@163.com>
2020-07-13 17:35:24 +0800

    Delete fanotify.c

--- fanotify.c
+++
@@ -1,3 +0,0 @@
-#define FAN_EVENT_OK(meta, len)	((long)(len) >= (long)FAN_EVENT_METADATA_LEN && \
-				(long)(meta)->event_len >= (long)FAN_EVENT_METADATA_LEN && \
-				(long)(meta)->event_len <= (long)(len))
